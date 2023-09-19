cde655b670a326f27cadc29b1ccd94b049356d81
Daniel Beserra Gomes <31704265+danielbgo@users.noreply.github.com>
2023-09-19 16:17:32 -0300

    Add files via upload

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,101 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo é Obrigatório!")
+	@Size(min = 5, max = 1000, message = "O atribute deve ter no minímo 5 e no máximo 1000 carácteres!")
+	private String nome;
+	
+	@NotBlank(message = "Descrição é Obrigatório!")
+	private String descricao;
+	
+	@NotBlank(message = "Laboratorio de origem do produto é obrigatório!")
+	private String laboratorio;
+	
+	private int quantidade;
+	
+	@NotNull(message = "Preço é obrigatório!")
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+
+}
